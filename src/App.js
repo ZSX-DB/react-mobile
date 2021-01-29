9b9773f70920b3a99eb28a2122b9f23f15e4b274
ZSX-DB <Sx82199609124535@outlook.com>
2021-01-29 13:32:10 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react";
-import { Cell, Checkbox } from 'zarm';
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+const App = () => {
     return (
-        <div>
-            <div className="test">
-                <span className="test-label">label</span>
-            </div>
-            <Cell><Checkbox>普通</Checkbox></Cell>
-            <Cell><Checkbox defaultChecked>默认选中</Checkbox></Cell>
-            <Cell><Checkbox disabled>禁用</Checkbox></Cell>
-            <Cell><Checkbox defaultChecked disabled>选中且禁用</Checkbox></Cell>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    React Mobile
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
         </div>
     )
-    // return (
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <p>
-    //         Edit <code>src/App.js</code> and save to reload.
-    //       </p>
-    //       <a
-    //         className="App-link"
-    //         href="https://reactjs.org"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         Learn React
-    //       </a>
-    //     </header>
-    //   </div>
-    // );
 }
 
-export default App;
+export default App
---
+++ src/view/home.js
@@ -0,0 +1,33 @@
+const Home = () => {
+    let count = 0
+    let startY, endY
+
+    document.addEventListener('touchstart', event => {
+        startY = event.touches[0].pageY
+    })
+
+    document.addEventListener('touchend', event => {
+        endY = event.changedTouches[0].pageY
+
+        // 当startY大于endY，向上滑动，反之向下移动
+        if (startY < endY && count > 0) {
+            count--
+        } else if (startY > endY && count < 3) {
+            count++
+        }
+        const {style, clientHeight} = document.querySelector('.one')
+        style.marginTop = `-${count * clientHeight}px`
+
+    })
+
+    return (
+        <div className="wrapper">
+            <div className="page one">Page1</div>
+            <div className="page two">Page2</div>
+            <div className="page three">Page3</div>
+            <div className="page four">Page4</div>
+        </div>
+    )
+}
+
+export default Home
