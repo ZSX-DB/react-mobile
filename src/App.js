b8b9abfa2fecdcfbb1164bc58778423c5d21e6be
ZSX-DB <Sx82199609124535@outlook.com>
2021-02-01 13:51:34 +0800

    update tabbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Switch, Redirect, Route} from "react-router-dom"
 import Portal from "@/view/portal"
+import Home from "@/view/home"
 import NotFound from "@/view/notFound"
 
 const App = () => (
@@ -7,6 +8,7 @@ const App = () => (
         <Switch>
             <Redirect exact from='/' to='/portal'/>
             <Route path='/portal' render={() => <Portal/>}/>
+            <Route path='/home' render={() => <Home/>}/>
             <Route path="*" render={() => <NotFound/>}/>
         </Switch>
     </BrowserRouter>
---
+++ src/view/home.js
@@ -0,0 +1,34 @@
+import {useState} from 'react';
+import {Icon, TabBar} from 'zarm'
+import '@/style/usual.scss'
+
+const Home = () => {
+    const [activeKey, setActiveKey] = useState('home')
+
+    return (
+        <div className="wrapper">
+
+            <TabBar
+                activeKey={activeKey}
+                onChange={setActiveKey}>
+                <TabBar.Item
+                    itemKey="home"
+                    title="home"
+                    icon={<Icon type="home"/>}
+                />
+                <TabBar.Item
+                    itemKey="store"
+                    title="store"
+                    icon={<Icon type="arrow-top"/>}
+                />
+                <TabBar.Item
+                    itemKey="mine"
+                    title="mine"
+                    icon={<Icon type="user"/>}
+                />
+            </TabBar>
+        </div>
+    );
+}
+
+export default Home
